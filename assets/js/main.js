cb8c9a22d1479801ca16780d4589b482fbce6ee6
danielplebani <danielpleba03@gmail.com>
2023-08-22 17:08:20 +0200

    sistem password and bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,23 @@
 const firstName = prompt('inserisci il tuo nome');
 console.log('il nome é ' + firstName);
 
-const firstNameInner = document.getElementById('firstName');
-firstNameInner .innerHTML = firstName ;
-
 
 //chiediamo all'utente il suo cognome e scriviamo in pagina la risposta
 const surname = prompt('inserisci il tuo cognome');
 console.log('il cognome é ' + surname);
-
-const surnameInner = document.getElementById('surname');
-surnameInner .innerHTML = surname ;
 
 
 //chiediamo all'utente il suo colore preferito e scriviamo in pagina la risposta
 const favColour = prompt('inserisci il tuo colore preferito');
 console.log('il colore preferito é ' + favColour);
 
-const favColourInner = document.getElementById('favColour');
-favColourInner .innerHTML = favColour + ' 23';
+
+//chiediamo all'utente il suo numero preferito e aggiungiamo 23 per creare la chiave
+const favNumber = prompt('inserisci il tuo numero preferito');
+console.log('il numero preferito é ' + favNumber);
+const key = Number(favNumber) + 23;
+
+
+//generiamo la password nella pagina html
+const password = document.getElementById('password');
+password.innerHTML=firstName + surname + favColour + key;
